309800f82c522e6ef9289ed936e55582e65243d9
Jasper Van der Jeugt <m@jaspervdj.be>
2019-02-26 16:03:54 -0500

    Bump deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -o errexit -o pipefail -o nounset
 
 mkdir -p summer.haskell.org/
-sshfs hsoc-site@www-origin.haskell.org: summer.haskell.org/
+sshfs hsoc-site@webhost.haskell.org: summer.haskell.org/
 cp -rv _site/* summer.haskell.org/htdocs/
 fusermount -u $PWD/summer.haskell.org
